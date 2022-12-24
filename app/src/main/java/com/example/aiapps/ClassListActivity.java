4c193d1b44fff93581961f5897c226e3d4d7ea82
2018331062 <52986013+MD-SUMON-MIAH@users.noreply.github.com>
2022-12-24 23:46:57 +0600

    change

--- app/src/main/java/com/example/aiapps/ClassListActivity.java
+++ app/src/main/java/com/example/aiapps/ClassListActivity.java
@@ -10,15 +10,33 @@ import android.widget.Button;
 public class ClassListActivity extends AppCompatActivity {
 
 
-    private Button AIButtn;
+    private Button AIButtn,CclassBtn,DSclassBtn;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_list);
+
         this.setTitle("Class List");
         AIButtn=findViewById(R.id.AI_class_Btn);
+        CclassBtn=findViewById(R.id.C_class_btn);
+        DSclassBtn=findViewById(R.id.DS_Class_Btn);
+
         AIButtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ClassListActivity.this,AudioInputActivity.class);
+                startActivity(intent);
+            }
+        });
+        CclassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ClassListActivity.this,AudioInputActivity.class);
+                startActivity(intent);
+            }
+        });
+        DSclassBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(ClassListActivity.this,AudioInputActivity.class);
