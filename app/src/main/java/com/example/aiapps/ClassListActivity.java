99f0bc5ff1535e9d6a86f7ad5e5f457c28edbf84
2018331062 <52986013+MD-SUMON-MIAH@users.noreply.github.com>
2022-12-24 15:09:25 +0600

    add audio ui

--- app/src/main/java/com/example/aiapps/ClassListActivity.java
+++ app/src/main/java/com/example/aiapps/ClassListActivity.java
@@ -2,15 +2,28 @@ package com.example.aiapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ClassListActivity extends AppCompatActivity {
 
+
+    private Button AIButtn;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_list);
+        AIButtn=findViewById(R.id.AI_class_Btn);
+        AIButtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ClassListActivity.this,AudioInputActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //sumon
     }
